c7edc23438dc89fd90ed77aaffb44551f89bacdc
Ali <khusniddinov1s@gmail.com>
2026-05-12 13:40:40 +0300

    Last control

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// ===========================
-// TEMA DEGISTIRME (Dark / Light)
-// ===========================
-
-// 1. Butonu sec
 const themeButton = document.querySelector('#theme-toggle');
 
-// 2. Butona tiklaninca body'ye "light" class'i ekle/cikar
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle('light');
 });
